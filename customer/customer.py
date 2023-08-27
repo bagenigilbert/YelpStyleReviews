fda3ab88a2afb1a0194bffbef7fc177771a205a3
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 10:10:52 +0300

    added instance attributes

--- customer/customer.py
+++ customer/customer.py
@@ -3,4 +3,6 @@ class Customer:
 #Initialize a customer object with given_name and family_name
     def __init__(self, given_name, family_name):
         #instances attributes
-        
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
