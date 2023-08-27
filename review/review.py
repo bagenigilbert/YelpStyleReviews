56c2798fb51612ad62691b50c7f5924b99dbea34
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 11:08:28 +0300

    added instance atrributes

--- review/review.py
+++ review/review.py
@@ -2,4 +2,9 @@
 class Review:
      # Initialize a Review object with a customer, restaurant, and rating
      def __init__(self, customer, restaurant, rating):
+          self.customer = customer
+          self.restaurant = restaurant
+          self.rating = rating
+
+          
           
