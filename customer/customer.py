7a5f489ed2ce63a0c670d53080b95635b1dcae45
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 11:07:34 +0300

    created review.py for customer restarant reviews

--- customer/customer.py
+++ customer/customer.py
@@ -10,10 +10,19 @@ class Customer:
     # methods to retrieve the given name of the customer
     def  get_given_name(self):
         return self.given_name
+    
     # method to retrieve the family name of the customer
     def get_family_name(self):
         return self.family_name
+    
     # method to retrieve the full name of the customer
     def get_full_name(self):
         return f"{self.given_name} {self.family_name}"
+    
+    # method to add a review for a restaurant with  a rating
+    def add_review(self,restaurant,rating):
+        review = review(self,restaurant,rating)  # Create a new Review instance
+        self.reviews.append(review)  # Add the review to the list of reviews for this customer
+        restaurant.add_review(review)  # Call the add_review method of the Restaurant class
+
     
---
+++ review/review.py
@@ -0,0 +1,5 @@
+# review
+class Review:
+     # Initialize a Review object with a customer, restaurant, and rating
+     def __init__(self, customer, restaurant, rating):
+          
