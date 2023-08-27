4ac10a670eb6dd587d1454997129abd8dd86cbec
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 16:57:15 +0300

    final push

--- customer/customer.py
+++ customer/customer.py
@@ -1,28 +1,29 @@
-#define the customer  class
+# Import the Review class from the review module
+from review.review import Review
+
+# Define the Customer class
 class Customer:
-#Initialize a customer object with given_name and family_name
+    # Constructor to initialize the customer object with given name and family name
     def __init__(self, given_name, family_name):
-        #instances attributes
         self.given_name = given_name
         self.family_name = family_name
-        self.reviews = [] # A list to store reviews left by this customer
+        self.reviews = []  # A list to store reviews left by this customer
 
-    # methods to retrieve the given name of the customer
-    def  get_given_name(self):
+    # Method to retrieve the given name of the customer
+    def get_given_name(self):
         return self.given_name
-    
-    # method to retrieve the family name of the customer
+
+    # Method to retrieve the family name of the customer
     def get_family_name(self):
         return self.family_name
-    
-    # method to retrieve the full name of the customer
+
+    # Method to retrieve the full name of the customer
     def get_full_name(self):
         return f"{self.given_name} {self.family_name}"
-    
-    # method to add a review for a restaurant with  a rating
-    def add_review(self,restaurant,rating):
-        review = review(self,restaurant,rating)  # Create a new Review instance
+
+    # Method to add a review for a restaurant with a rating
+    def add_review(self, restaurant, rating):
+        # Create a new Review instance using the Review class
+        review = Review(self, restaurant, rating)
         self.reviews.append(review)  # Add the review to the list of reviews for this customer
         restaurant.add_review(review)  # Call the add_review method of the Restaurant class
-
-    
--- review/review.py
+++ review/review.py
@@ -1,24 +1,19 @@
-# review
+# Define the Review class
 class Review:
-     # Initialize a Review object with a customer, restaurant, and rating
-     def __init__(self, customer, restaurant, rating):
-          self.customer = customer
-          self.restaurant = restaurant
-          self.rating = rating
+    # Constructor to initialize a review with a customer, restaurant, and rating
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer  # The customer who left the review
+        self.restaurant = restaurant  # The restaurant that was reviewed
+        self.rating = rating  # The rating given to the restaurant
 
-     # Method to retrieve the rating of the review
-     def get_rating(self):
-          return self.rating
-     
-     # Method to retrieve the customer who left the review
-     def get_customer(self):
-          return self.customer
-     
+    # Method to retrieve the rating of the review
+    def get_rating(self):
+        return self.rating
+
+    # Method to retrieve the customer who left the review
+    def get_customer(self):
+        return self.customer
+
     # Method to retrieve the restaurant that was reviewed
-def get_restaurant(self):
-     return self.restaurant
-
-
-
-
-          
+    def get_restaurant(self):
+        return self.restaurant
