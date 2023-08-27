ca386fba863082a01580d2b02e8ab696bc23b64c
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 11:13:59 +0300

    added instance attribute under constractor

--- restaurant/restaurant.py
+++ restaurant/restaurant.py
@@ -1,4 +1,7 @@
 # restaurant
 
 class Restaurant:
-    
+    # Initialize a Restaurant object with a name
+    def __init__(self, name):
+        self.name = name
+        self.reviews = []  # A list to store reviews for this restaurant
