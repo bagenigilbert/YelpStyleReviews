62735d1d43d378c09a9e42cd633f3f712e80b881
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-27 10:22:42 +0300

    added methods for given name and family name

--- customer/customer.py
+++ customer/customer.py
@@ -5,4 +5,11 @@ class Customer:
         #instances attributes
         self.given_name = given_name
         self.family_name = family_name
-        self.reviews = []
+        self.reviews = [] # A list to store reviews left by this customer
+
+    # methods to retrieve the given name of the customer
+    def  get_given_name(self):
+        return self.given_name
+    # method to retrieve the family name of the customer
+    def get_family_name(self):
+        return self.family_name
